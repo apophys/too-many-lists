6a946f27b4e620aed4d2d7301180c4c29e5a7fa1
Milan Kubík <mkubik@redhat.com>
2017-04-16 23:07:36 +0200

    Add push method

--- src/first.rs
+++ src/first.rs
@@ -1,5 +1,7 @@
 // implementation of first linked list
 
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -19,5 +21,14 @@ impl List {
 
     pub fn new() -> Self {
         List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
     }
 }
