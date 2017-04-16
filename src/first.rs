5b1f94e5fe7089008b56f55f5e7f9c53f2d346b8
Milan Kubík <mkubik@redhat.com>
2017-04-16 23:49:43 +0200

    Implement Drop trait for the list

--- src/first.rs
+++ src/first.rs
@@ -16,7 +16,6 @@ enum Link {
     More(Box<Node>),
 }
 
-
 impl List {
 
     pub fn new() -> Self {
@@ -40,6 +39,16 @@ impl List {
                 self.head = node.next;
                 Some(node.elem)
             }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
         }
     }
 }
